40b665a1cbe5ed0a4f1376d42c041384cd8185a0
fs0 <schumacherf89@gmail.com>
2013-12-18 15:30:20 +0100

    initial commit

---
+++ 01to10.hs
@@ -0,0 +1,34 @@
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast [x,_] = x
+myButLast (x:xs) = myButLast xs
+
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "not in range"
+elementAt (x:xs) 1 = x
+elementAt (x:xs) i = elementAt xs (i-1)
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = myReverse xs == xs
+
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
+flatten (List []) = []
+flatten (Elem a) = [a]
+flatten (List (x:xs)) = flatten x ++ (flatten (List xs))
+
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:(y:xs)) = if (x==y) then compress (x:xs) else x:(compress (y:xs))
---
+++ README.md
@@ -0,0 +1,4 @@
+99 Haskell Problems
+===================
+
+http://www.haskell.org/haskellwiki/H-99:_Ninety-Nine_Haskell_Problems
