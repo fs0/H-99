83be960fd1691d267162afbddc2df2b086007d4b
fs0 <schumacherf89@gmail.com>
2013-12-27 14:16:51 +0100

    problem 11

---
+++ 11to20.hs
@@ -0,0 +1,13 @@
+data Number a = Single a | Multiple Int a deriving Show
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = (x : (takeWhile (==x) xs)) : pack (dropWhile (==x) xs)
+
+encodeModified :: (Eq a) => [a] -> [Number a]
+--encodeModified = map (\x -> if (length x) == 1 then (Single (x!!0)) else (Multiple (length x) (x!!0))) . pack
+encodeModified = map checkNumber . map (\x -> (length x, x!!0)) . pack
+    where
+        checkNumber (1,c) = Single c
+        checkNumber (x,c) = Multiple x c
+
